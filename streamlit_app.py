3317a92f553399d08fd405a9c6db09541ab2886e
srushtikumbhare2023 <srushtikumbhare2023@gmail.com>
2025-10-06 22:39:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import requests
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -16,7 +17,9 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
-fruit_options = my_dataframe.to_pandas()['FRUIT_NAME'].tolist()
+pd_df = my_dataframe.to_pandas()
+st.my_dataframe(pd_df)
+st.stop()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
      my_dataframe,
