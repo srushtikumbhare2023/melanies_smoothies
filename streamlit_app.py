421956245ec2aa8e0adfac091688ad42b545059a
srushtikumbhare2023 <srushtikumbhare2023@gmail.com>
2025-10-07 12:18:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,11 +17,11 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-st.dataframe(data=pd_df, use_container_width=True)
+st.dataframe(pd_df)
+#st.dataframe(data=pd_df, use_container_width=True)
 #st.stop()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -29,31 +29,21 @@ ingredients_list = st.multiselect(
      max_selections = 5,
     )
 if ingredients_list:
-    ingredients_string = " ".join(ingredients_list)
+   ingredients_string = ''
     for fruit_chosen in ingredients_list:
-        #ingredients_string += fruit_chosen + ' '
+        ingredients_string += fruit_chosen + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + 'Nutrition Information')
-        # smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
-        #sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-    #st.write(ingredients_list)
-        try:
-            smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
-            smoothiefroot_response.raise_for_status()
-            st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-        except Exception:
-            st.warning(f"Cannot fetch data for {fruit_chosen} — Internet access is blocked inside Snowflake.")
-    #my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             #values ('""" + ingredients_string + """','""" + name_on_order +""""')"""
     #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert = st.button('Submit Button')
     if time_to_insert:
-        #session.sql(my_insert_stmt).collect()
-        session.table("smoothies.public.orders").insert(
-            Row(INGREDIENTS=ingredients_string, NAME_ON_ORDER=name_on_order)
-        )
+        session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
   
 
