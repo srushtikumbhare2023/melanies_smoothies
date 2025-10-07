4b12fc22267505931da13a17746e24bb52dc0139
srushtikumbhare2023 <srushtikumbhare2023@gmail.com>
2025-10-07 11:32:51 +0530

    Update streamlit_app.py
    missing ,

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ pd_df = my_dataframe.to_pandas()
 #st.stop()
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
-     pd_df["FRUIT_NAME"]
+     pd_df["FRUIT_NAME"],
      max_selections = 5,
     )
 if ingredients_list:
